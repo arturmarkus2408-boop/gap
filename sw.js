71d2cffcda0b16d3f3e9d7c1333cc3f097ef720c
arturmarkus2408-boop <arturmarkus2408@gmail.com>
2026-05-25 12:31:40 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gap-v1';
+const CACHE = 'gap-v2';
 const ASSETS = ['./index.html', './manifest.json', './sw.js'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
